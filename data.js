67807859d580cb67ef50c3a08d669e45e259c9d8
Saqib Ahmed <87967028+SaqibAarman@users.noreply.github.com>
2021-09-15 14:03:09 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'Chain_Smoker',
+        path: 'musics/song1.mp3',
+        artist: 'Vidya Vox',
+        cover: 'images/ChainSmoker.jpg',
+    },
+    {
+        name: 'Mercy',
+        path: 'musics/song2.mp3',
+        artist: 'BadShah',
+        cover: 'images/Mercy.jpg',
+    },
+    {
+        name: 'Wada Tha Har',
+        path: 'musics/song3.mp3',
+        artist: 'Tonny Kakkar',
+        cover: 'images/Wada.jpg',
+    },
+    {
+        name: 'Mere Rang Me',
+        path: 'musics/song4.mp3',
+        artist: 'Surya Veer',
+        cover: 'images/Mere-Rang-me.jpg',
+    },
+    {
+        name: 'Nashe Se Chadh',
+        path: 'musics/song5.mp3',
+        artist: 'Arjith Singh',
+        cover: 'images/Nashe-Si-Chadh-Gayi.jpg',
+    },
+    {
+        name: 'Give Me Some',
+        path: 'musics/song6.mp3',
+        artist: 'Sharman Joshi',
+        cover: 'images/Give-Me-Some.jpg',
+    },
+    {
+        name: 'Let Me Love',
+        path: 'musics/song7.mp3',
+        artist: 'Vidya Vox',
+        cover: 'images/Let-Me-Love.jpg',
+    },
+]
